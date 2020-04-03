00c5acd56eb7fecac71d1fd552471fbc75cc205f
Damian Kolaska <damian.kolaska99@gmail.com>
2020-04-03 20:50:42 +0200

    zadanie2

--- main.py
+++ main.py
@@ -7,3 +7,19 @@ app = FastAPI()
 @app.get("/")
 def root():
 	return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/method")
+def get_method():
+	return {"method": "GET"}
+
+@app.post("/method")
+def get_method():
+	return {"method": "POST"}
+
+@app.delete("/method")
+def get_method():
+	return {"method": "DELETE"}
+
+@app.put("/method")
+def get_method():
+	return {"method": "PUT"}
