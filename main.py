4c3fc30c9902c34d831a0fd7c567dea530b31b85
GRO4T <damian.kolaska99@gmail.com>
2020-04-24 20:26:55 +0200

    2

--- main.py
+++ main.py
@@ -58,11 +58,13 @@ def login(response: Response, credentials: HTTPBasicCredentials = Depends(securi
 
 @app.post("/logout")
 def logout(response: Response, session_token: str = Cookie(None)):
+    """
     if session_token not in app.sessions:
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Unauthorised"
         )
+        """
     app.sessions.remove(session_toker)
     response.headers["Location"] = "/"
     response.status_code = status.HTTP_307_TEMPORARY_REDIRECT
