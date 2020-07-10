2100c7946683ff95175d0af0d4bbbeb677b2e1b3
GRO4T <damian.kolaska99@gmail.com>
2020-07-10 19:05:19 +0200

    Credentials are now safely extracted from the OS environmental variables

--- app/database/database.py
+++ app/database/database.py
@@ -5,22 +5,11 @@ import sqlite3
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-import yaml
+import os
 
 connection: sqlite3.Connection
 
-SQLALCHEMY_DATABASE_URL = '';
-
-# load postgres url from an yaml file
-postgres_conf = {}
-try:
-    with open('app/database/postgres_url_local.yml') as yaml_file:
-        postgres_conf = yaml.load(yaml_file.read(), Loader=yaml.SafeLoader)
-except FileNotFoundError:
-    with open('app/database/postgres_url.yml') as yaml_file:
-        postgres_conf = yaml.load(yaml_file.read(), Loader=yaml.SafeLoader)
-SQLALCHEMY_DATABASE_URL = postgres_conf.get("postgres").get("url")
-print(SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.environ['DATABASE_URL']
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
